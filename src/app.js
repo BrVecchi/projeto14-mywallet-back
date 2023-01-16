c17a2a156cc6633a27e31e668ad36959e51f4ff2
BrVecchi <bno.vecchi@gmail.com>
2023-01-16 15:59:48 -0300

    refactor app js

--- src/app.js
+++ src/app.js
@@ -8,5 +8,7 @@ app.use(cors());
 app.use(router)
 
 
-app.listen(5000);
-console.log("Rodando na porta 5000");
+const port = process.env.PORT || 5000
+
+app.listen(port);
+console.log(`Rodando na porta ${port}`);
